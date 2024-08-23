d079ebdfb1b1dee354aaf672502381424c36e8da
IhGabeZ <mts.c@live.com>
2024-08-23 12:37:47 -0300

    Update-main.py
    Major Update, many new func implemented.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import os
 from rich import print_json
 from colorama import init, Fore, Style
+import time
+from datetime import datetime
 
 def create_user(subdomain, token, login, first_name, last_name, phone, email):
     url = f'https://{subdomain}.norago.tv/apex/v2/subscribers/create'
@@ -315,18 +317,138 @@ def print_json_color(json_data):
     # A função print_json do rich cuida da formatação e coloração automaticamente.
     print_json(data=json_data)
 
+def get_full_info(subdomain, token, login):
+    file_path = 'all_users.json'
+    i = 0
+
+    # Abrindo e lendo o arquivo JSON
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    # Dicionário para armazenar o novo JSON
+    new_data = {}
+
+    # Iterando sobre cada item na lista
+    for item in data:
+        last_name = item.get('lastName')
+        account_number = item.get('accountNumber')
+        i += 1
+        
+        if i % 15 == 0:
+            time.sleep(1)
+        
+        try:
+            # Obtendo a resposta para cada item
+            response = get_sub_info(subdomain, token, login, last_name, account_number)
+            
+            # Extraindo as informações desejadas até "devices"
+            result = response.get('result')
+            
+            if result is None:
+                raise ValueError(f"Resposta inesperada: 'result' não encontrado para {account_number}")
+            
+            # Mantendo apenas informações até "devices"
+            filtered_result = {}
+            for key, value in result.items():
+                filtered_result[key] = value
+                if key == 'devices':
+                    break
+            
+            # Adicionando o resultado ao novo dicionário com o "accountNumber" como chave
+            new_data[account_number] = filtered_result
+        
+        except Exception as e:
+            # Tratamento de erro: continue a execução e exiba o erro e o account_number
+            print(f"Erro ao processar {account_number}: {e}")
+            continue
+
+    # Escrevendo o novo JSON em um arquivo
+    with open('filtered_users.json', 'w') as new_file:
+        json.dump(new_data, new_file, indent=4)
+
+    print("Novo arquivo JSON criado: filtered_users.json")
+
+def credit_time_to_everyone(subdomain, token, login, subscription_id):
+    file_path = 'all_users_full.json'
+    filtered_users = {}
+
+    # Abrindo e lendo o arquivo JSON
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    # Iterando sobre cada item no JSON e filtrando por data
+    for account_number, user_info in data.items():
+        last_name = user_info.get('lastName')
+        expiration_time = user_info.get('expirationTime')
+        email = user_info.get('email')
+
+        if expiration_time:
+            date = datetime.strptime(expiration_time, "%Y-%m-%dT%H:%M:%SZ")
+            if date.year == 2024 and date.month == 1 and date.day == 25:
+                filtered_users[account_number] = {"lastName": last_name, "email": email}
+                print(f"Account Number {account_number}, Last Name {last_name}, Expiration Time: {expiration_time}, Email {email}")
+                make_payment(subdomain, token, login, account_number, last_name, subscription_id)
+
+
+    # Contando o número total de usuários filtrados
+    total_users = len(filtered_users)
+
+    # Criando o dicionário final que inclui o total de usuários
+    final_output = {
+        "totalUsers": total_users,
+        "users": filtered_users
+    }
+
+    # Salvando o novo JSON com os usuários filtrados e o total de usuários
+    with open('receivedTimeUsers.json', 'w') as new_file:
+        json.dump(final_output, new_file, indent=4)
+
+def extractSubscriberFromDataX(day, month, year):
+    file_path = 'all_users_full.json'
+    filtered_users = {}
+
+    # Abrindo e lendo o arquivo JSON
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    # Iterando sobre cada item no JSON e filtrando por data
+    for account_number, user_info in data.items():
+        last_name = user_info.get('lastName')
+        expiration_time = user_info.get('expirationTime')
+        email = user_info.get('email')
+
+        if expiration_time:
+            date = datetime.strptime(expiration_time, "%Y-%m-%dT%H:%M:%SZ")
+            if date.year == 2024:# and date.month == 1: #and date.day == 28:
+                filtered_users[account_number] = {"lastName": last_name, "email": email}
+                print(f"Account Number {account_number}, Last Name {last_name}, Expiration Time: {expiration_time}, Email {email}")
+
+    # Salvando os sobrenomes e emails em um arquivo de texto
+    with open('receivedTimeUsers.txt', 'a') as file:
+        for account_number, user_info in filtered_users.items():
+            last_name = user_info.get('lastName')
+            email = user_info.get('email')
+            file.write(f"{last_name}\n{email}\n")
+
+    # Contando o número total de usuários filtrados
+    total_users = len(filtered_users)
+
+    # Criando o dicionário final que inclui o total de usuários
+    final_output = {
+        "totalUsers": total_users,
+        "users": filtered_users
+    }
+
+    # Salvando o novo JSON com os usuários filtrados e o total de usuários
+    with open('receivedTimeUsers.json', 'w') as new_file:
+        json.dump(final_output, new_file, indent=4)
+
 # Auth Setup
 subdomain = os.getenv('noraSub')
 token = os.getenv('token')
 login = os.getenv('login')
 
-
-# User Info Test Setup 
-first_name = 'IhGabeZ'
-last_name = 'Cord'
-phone = '5099969999'
-email = 'IhGabeZ@IhGabeZ.com'
-account_number = 'SGO300230'
+subID = "4SDBdAD5"
 
 # Program Start 
 def main_menu():
@@ -344,80 +466,133 @@ def main_menu():
         print(Fore.YELLOW + "9. " + Fore.WHITE + "Get Packages Info")
         print(Fore.YELLOW + "10. " + Fore.WHITE + "Get Network Info")
         print(Fore.YELLOW + "11. " + Fore.WHITE + "Search/Save All")
-        print(Fore.RED + "12. " + Fore.WHITE + "Exit")
+        print(Fore.YELLOW + "12. " + Fore.WHITE + "Get Everyone Full Info")
+        print(Fore.YELLOW + "13. " + Fore.WHITE + "Credit Time to Every1")
+        print(Fore.YELLOW + "14. " + Fore.WHITE + "Extract Subscriber from Data X")
+        print(Fore.RED + "15. " + Fore.WHITE + "Exit")
         print(Style.RESET_ALL)
         
-        choice = input(Fore.GREEN + "Escolha uma opção (1-12): " + Style.RESET_ALL)
+        choice = input(Fore.GREEN + "Options (1-14): " + Style.RESET_ALL)
         
         if choice == '1':
             # Criar Usuário
-            first_name, last_name, phone, email = input("Digite first_name last_name phone email: ").split()
+            os.system("cls")
+            first_name, last_name, phone, email = input(Fore.MAGENTA + "Type First Name " + Fore.RESET + Fore.GREEN + "Last Name " + Fore.RESET + Fore.YELLOW + "Phone " + Fore.RESET + Fore.CYAN + "Email " + Fore.RESET).split()
             response = create_user(subdomain, token, login, first_name, last_name, phone, email)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
         
         elif choice == '2':
             # Buscar Assinante
-            email = input("Digite email: ")
+            email = input(Fore.CYAN + "Type Email " + Fore.RESET)
             response = search_subscriber(subdomain, token, login, email)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
         
         elif choice == '3':
             # Fazer Pagamento
-            account_number, last_name, subscription_id = input("Digite account_number last_name subscription_id: ").split()
+            os.system("cls")
+            account_number, last_name, subscription_id = input(Fore.GREEN + "Type Account Number " + Fore.RESET + Fore.MAGENTA + "Last Name " + Fore.RESET + Fore.YELLOW + "Subscription ID " + Fore.RESET).split()
             response = make_payment(subdomain, token, login, account_number, last_name, subscription_id)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
         
         elif choice == '4':
+            os.system("cls")
             # Obter Informações do Assinante
-            last_name, account_number = input("Digite last_name account_number: ").split()
+            last_name, account_number = input(Fore.MAGENTA + "Type Last Name " + Fore.RESET + Fore.GREEN + "Account Number " + Fore.RESET).split()
             response = get_sub_info(subdomain, token, login, last_name, account_number)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
         
         elif choice == '5':
+            os.system("cls")
             # Remover Dispositivo
-            last_name, account_number, device_number = input("Digite last_name account_number device_number: ").split()
+            last_name, account_number, device_number = input(Fore.MAGENTA + "Type Last Name " + Fore.RESET + Fore.GREEN + "Account Number " + Fore.RESET + Fore.YELLOW + "Device Number " + Fore.RESET).split()
             response = remove_device(subdomain, token, login, last_name, account_number, device_number)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
         
         elif choice == '6':
+            os.system("cls")
             # Excluir Assinante
-            last_name, account_number = input("Digite last_name account_number: ").split()
+            last_name, account_number = input(Fore.MAGENTA + "Type Last Name " + Fore.RESET + Fore.GREEN + "Account Number " + Fore.RESET).split()
             response = delete_subscriber(subdomain, token, login, last_name, account_number)
             print_json_color(response)
-        
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
         elif choice == '7':
+            os.system("cls")
             # Remover Todos os Dispositivos
-            last_name, account_number = input("Digite last_name account_number: ").split()
+            last_name, account_number = input(Fore.MAGENTA + "Type Last Name " + Fore.RESET + Fore.GREEN + "Account Number " + Fore.RESET).split()
             remove_all_devices(subdomain, token, login, last_name, account_number)
-        
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
         elif choice == '8':
+            os.system("cls")
             # Renovar Todos os Códigos de Ativação
-            last_name, account_number = input("Digite last_name account_number: ").split()
+            last_name, account_number = input(Fore.MAGENTA + "Type Last Name " + Fore.RESET + Fore.GREEN + "Account Number " + Fore.RESET).split()
             link_codes = renew_all_activation_codes(subdomain, token, login, last_name, account_number)
             if link_codes:
                 print("Link Codes:")
                 for code in link_codes:
                     print(f"Code : {code}")
-        
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
         elif choice == '9':
+            os.system("cls")
             # Obter Informações dos Pacotes
             response = get_packages_info(subdomain, token, login)
             print_json_color(response)
-        
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
         elif choice == '10':
+            os.system("cls")
             # Obter Informações da Rede
             response = get_network_info(subdomain, token, login)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
 
         elif choice == '11':
+            os.system("cls")
             # Obter Informações da Rede
             response = search_all(subdomain, token, login)
             print_json_color(response)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
 
         elif choice == '12':
-            # Sair
+            os.system("cls")
+            get_full_info(subdomain, token, login)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
+        elif choice == '13':
+            os.system("cls")
+            credit_time_to_everyone(subdomain, token, login, subID)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
+        elif choice == '14':
+            os.system("cls")
+            day, month, year = input(Fore.CYAN + "Type DAY " + Fore.RESET + Fore.GREEN + "MONTH " + Fore.RESET + Fore.YELLOW + "YEAR " + Fore.RESET).split()
+            extractSubscriberFromDataX(day, month, year)
+            input(Fore.RED + "Press enter to continue")
+            os.system("cls")
+
+        elif choice == '15':
             break
-        
+
         else:
             print("Opção inválida, tente novamente.")
 
